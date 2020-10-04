0ea8ca0ad8854a36c3e4cfd9c421c1e04b80818a
LubavaSv <lubava.svistova1@gmail.com>
2020-10-05 01:38:20 +0300

    validate user creation data

---
+++ src/users/dto/create.user.dto.ts
@@ -0,0 +1,16 @@
+import { IsEmail, IsNotEmpty, IsString, MaxLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(20)
+  name: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+}
