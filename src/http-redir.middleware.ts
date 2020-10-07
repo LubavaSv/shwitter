f3241050f9c45522cc5f96f11a8e72083627f89a
LubavaSv <lubava.svistova1@gmail.com>
2020-10-07 19:18:10 +0300

    fix redirection middleware

--- src/http-redir.middleware.ts
+++ src/http-redir.middleware.ts
@@ -3,15 +3,15 @@ import { Injectable, NestMiddleware } from '@nestjs/common';
 @Injectable()
 export class HttpRedirMiddleware implements NestMiddleware {
   use(req: any, res: any, next: () => void) {
-    console.log('m');
     if (!req.secure) {
       res.redirect(
+        307,
         'https://' +
           req.headers.host.replace(
             `${process.env.PORT}`,
             `${process.env.SECURE_PORT}`,
           ) +
-          req.url,
+          req.baseUrl,
       );
     } else {
       next();
